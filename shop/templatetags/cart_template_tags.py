6c8d22c06cb49f67fc6257f66614b1c8d60ec327
Barbaro de la Rosa <barbarodelarosa@gmail.com>
2022-03-01 00:46:39 -0500

    Ver productos en el carrito de navegacion

--- shop/templatetags/cart_template_tags.py
+++ shop/templatetags/cart_template_tags.py
@@ -8,3 +8,17 @@ def cart_item_count(request):
     order = get_or_set_order_session(request)
     count = order.items.count()
     return count
+
+
+@register.filter
+def cart_item_order(request):
+    order = get_or_set_order_session(request)
+    items = order.items.all()
+    return items
+
+@register.filter
+def cart_item_order_subtotal_price(request):
+    order = get_or_set_order_session(request)
+    total = order.get_subtotal()
+    return total
+
