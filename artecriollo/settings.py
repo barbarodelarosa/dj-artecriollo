a01db922ba2fa18db8b4339fcfe05d597fb74101
Barbaro de la Rosa <barbarodelarosa@gmail.com>
2022-06-07 09:35:50 -0400

    2

--- artecriollo/settings.py
+++ artecriollo/settings.py
@@ -189,8 +189,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),'/home/cloud/artecriollo/static/',]
 # STATIC_URL = '/media/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
