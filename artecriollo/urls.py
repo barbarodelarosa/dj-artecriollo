2a9c55cd92f80d1e49544198597b9a420bcd2e0c
Barbaro de la Rosa <barbarodelarosa@gmail.com>
2022-06-07 07:34:39 -0400

    url static and media1234

--- artecriollo/urls.py
+++ artecriollo/urls.py
@@ -42,11 +42,11 @@ urlpatterns = [
 
 
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # if settings.DEBUG:
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 handler404 = views.pag_404_not_found
