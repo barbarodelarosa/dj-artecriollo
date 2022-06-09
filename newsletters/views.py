7fd2a73e6d66035859626eb8604d87dc74f8f458
Barbaro de la Rosa <barbarodelarosa@gmail.com>
2022-06-09 17:34:48 -0400

    Config WEBPUSH

--- newsletters/views.py
+++ newsletters/views.py
@@ -95,11 +95,10 @@ def newsletter_signup(request):
             message_admin.send()
 
             messages.success(request, 'Hemos enviado un correo electrónico a su cuenta confirmando su inscripción')
-    if request.user.is_authenticated:
-        messages.success(request, 'Esta autenticado...')
-        user = get_object_or_404(User, pk=request.user.id)
-        payload = {'head': "DATA ENVIADA", 'body': "MENSAJE ENVIADO"}
-        send_user_notification(user=user, payload="payload", ttl=1000)    
+    messages.success(request, 'Esta autenticado...')
+    user = get_object_or_404(User, pk=request.user.id)
+    payload = {'head': "DATA ENVIADA", 'body': "MENSAJE ENVIADO"}
+    send_user_notification(user=user, payload="payload", ttl=1000)    
 
     messages.warning(request, 'ANTES DE REENVIAR')
 
