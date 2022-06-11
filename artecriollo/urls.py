92a0b0673ad14e93abc8032aa7b805d49f3b5e94
Barbaro de la Rosa <barbarodelarosa@gmail.com>
2022-06-10 23:54:08 -0400

    Config and Add sitemaps

--- artecriollo/urls.py
+++ artecriollo/urls.py
@@ -15,7 +15,7 @@ from django.views.decorators.cache import cache_page
 
 sitemaps = {
     'static': StaticViewSitemap,
-    'category-shop': CategoryShopSitemap,
+    # 'category-shop': CategoryShopSitemap,
 }
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -46,10 +46,15 @@ urlpatterns = [
     # path('', RedirectView.as_view(pattern_name='home'), name='redirect-register'),
     path('', include('pwa.urls')),
     # path('sitemap.xml', views_sitemap.sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('sitemap.xml', cache_page(86400)(views_sitemap.index), {'sitemaps': sitemaps, 'sitemap_url_category_shop': 'category-shop'}),
-    path('sitemap-category-shop.xml', cache_page(86400)(views_sitemap.sitemap), {'sitemaps': sitemaps}, name='category-shop'),
+    # path('sitemap.xml', cache_page(86400)(views_sitemap.index), {'sitemaps': sitemaps, 'sitemap_url_category_shop': 'category-shop'}),
+    # path('sitemap-category-shop.xml', cache_page(86400)(views_sitemap.sitemap), {'sitemaps': sitemaps}, name='category-shop'),
     
-
+    path('sitemap.xml',
+         cache_page(86400)(views_sitemap.index),
+         {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
+    path('sitemap-<section>.xml',
+         cache_page(86400)(views_sitemap.sitemap),
+         {'sitemaps': sitemaps}, name='sitemaps'),
 
 
     
